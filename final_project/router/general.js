6d15df8303c52553ee588447708789714a3236b3
nickdenny <123418391+nickdenny@users.noreply.github.com>
2024-05-19 16:36:53 -0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,7 +7,97 @@ const public_users = express.Router();
 const doesExist = (username)=>{
     let userswithsamename = users.filter((user)=>{
       return user.username === username
+    })const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+const app = express();
+public_users.use(express.urlencoded({ extended: true }));
+
+const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
     });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  const authenticatedUser = (username,password)=>{
+    let validusers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+public_users.post("/register", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username && password) {
+        if (!doesExist(username)) {
+            users.push({"username":username,"password":password});
+            return res.status(200).json({message: "User successfully registred. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }/* else {
+        return res.status(404).json({message: "Please provide username and password!"});
+    }*/
+    return res.status(404).json({message: req.body.username + "Unable to register user."});
+});
+
+// Get the book list available in the shop
+public_users.get('/',function (req, res) {
+    res.send(books);
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
+ });
+
+// Get book details based on author
+public_users.get('/author/:author',function (req, res) {
+    const author = req.params.author;
+    let bookArray = {};
+
+    for (let key in books){
+        if(books[key].author == author){
+            bookArray[key] = books[key];
+        }
+    }
+    res.send(bookArray);
+});
+
+// Get all books based on title
+public_users.get('/title/:title',function (req, res) {
+    const title = req.params.title;
+    let bookArray = {};
+
+    for (let key in books){
+        if(books[key].title == title){
+            bookArray[key] = books[key];
+        }
+    }
+    res.send(bookArray);
+});
+
+//  Get book review
+public_users.get('/review/:isbn',function (req, res) {
+    const isbn = req.params.isbn;
+    res.send(books[isbn]["reviews"]);
+});
+
+module.exports.general = public_users;
+
     if(userswithsamename.length > 0){
       return true;
     } else {
