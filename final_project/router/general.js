2a65f7b8f1ea59678722e1dd8163827fdc7e2609
nickdenny <123418391+nickdenny@users.noreply.github.com>
2024-05-19 12:20:23 -0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,42 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  const authenticatedUser = (username,password)=>{
+    let validusers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username && password) {
+        if (!doesExist(username)) {
+            users.push({"username":username,"password":password});
+            return res.status(200).json({message: "User successfully registred. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }/* else {
+        return res.status(404).json({message: "Please provide username and password!"});
+    }*/
+    return res.status(404).json({message: req.body.username + "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -51,7 +83,7 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    res.send(books[isbn].reviews);
+    res.send(books[isbn]["reviews"]);
 });
 
 module.exports.general = public_users;
